afce0aa6f009655c2b5cefac598839f7cc3e2ac7
Irene Scott <irenejinscott@gmail.com>
2020-06-11 23:38:24 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.pluck(:name).map(&:to_s)
+    self.artists.pluck(:name).map(:to_s)
   end
 end
